f750a4039b2a9f5a6c98130fec9fa36dae9e59b8
Nene1724 <22520937@gm.uit.edu.vn>
2024-04-29 13:31:36 +0700

    fix

--- QuanLySinhVien/QuanLySinhVien/Teacher/ThongKe.cs
+++ QuanLySinhVien/QuanLySinhVien/Teacher/ThongKe.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Windows.Forms;
 using Google.Cloud.Firestore;
-using Google.Cloud.Firestore.V1;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace QuanLySinhVien
 {
